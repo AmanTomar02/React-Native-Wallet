9be830d7e7619fb17e5cc498f52d3b4c222bb6eb
Aman Tomar <at8871790@gmail.com>
2025-11-08 13:06:08 +0530

    Initial commit

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,1 +1,3 @@
-export const API_URL = "https://wallet-api-v2r2.onrender.com";
+// export const API_URL = "https://wallet-api-v2r2.onrender.com";
+import Constants from "expo-constants";
+export const API_URL = Constants.expoConfig.extra.API_URL;
